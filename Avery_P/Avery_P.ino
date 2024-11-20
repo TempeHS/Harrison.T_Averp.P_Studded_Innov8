db0928388fbba1b832935850430f840a2461690f
Avery P <159206600+AveryPilcer@users.noreply.github.com>
2024-11-21 10:30:25 +1100

    setup my ultrasonic sensor

--- Avery_P/Avery_P.ino
+++ Avery_P/Avery_P.ino
@@ -1,2 +1,12 @@
-#include <Ultrasonic.h>
+#include "Ultrasonic.h"
+Ultrasonic mysensor(5);
+//Pin 5 is for ultrasonic
+int buttonPin = 8
+int buttonState = 0
+void setup() {
+  Serial.begin(9600);
+}
 
+void loop() {
+  Serial.println(mysensor.distanceRead());
+}
