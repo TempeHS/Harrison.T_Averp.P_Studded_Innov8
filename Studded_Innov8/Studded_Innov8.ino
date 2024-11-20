9f7d83a2c6953c0ef4f9f8c89a085080294d8ebb
Avery P <159206600+AveryPilcer@users.noreply.github.com>
2024-11-20 12:25:56 +1100

    List of what we needed for the stuff

---
+++ Studded_Innov8/Studded_Innov8.ino
@@ -0,0 +1,10 @@
+pin8 = buttonPin
+
+https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue/Sensor_Kit/Servo_Motor
+Servo Motors x7
+
+https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue/Sensor_Kit/Ultrasonic_Ranger
+UltraSonic Ranger x1
+
+https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue/Sensor_Kit/Button
+Button x1
